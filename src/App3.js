f2ee5c7951f4aaaa756ecce4a89c398eeef4fb39
Aidas <aidas004@gmail.com>
2021-12-22 10:05:31 +0200

    10:05

--- src/App3.js
+++ src/App3.js
@@ -2,18 +2,30 @@ import './App3.css'
 import Home from "./components3/Home";
 import Shop from "./components3/Shop";
 import Cart from "./components3/Cart";
+import {useState} from "react";
 
 function App3 () {
+
+    const [window, setWindow] = useState(0)
+    let showWindow;
+    if (window === 0) {
+        showWindow = <Home />
+    }
+    if (window === 1) {
+        showWindow = <Shop />
+    }
+    if (window === 2) {
+        showWindow = <Cart />
+    }
     return (
         <div className="App">
             <div className="toolBar">
-                <a href="#">HOME</a>
-                <a href="#">SHOP</a>
-                <a href="#">CART</a>
+                <button onClick={() => setWindow(0)}>HOME</button>
+                <button onClick={() => setWindow(1)}>SHOP</button>
+                <button onClick={() => setWindow(2)}>CART</button>
+
             </div>
-            <Home/>
-            <Shop/>
-            <Cart/>
+            {showWindow}
         </div>
     )
 }
